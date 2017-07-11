1c51ce253557d57ba14e458b2759d3bec0361f7c
songce <18641170048@163.com>
2017-07-11 21:59:54 +0800

    init

---
+++ src/main/java/springbootdocker_war/SpringbootDockerWarApplication.java
@@ -0,0 +1,12 @@
+package springbootdocker_war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerWarApplication.class, args);
+	}
+}
